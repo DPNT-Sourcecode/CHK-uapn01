9b89a600c0e103f034b09100a2bd9d571f1a83ae
sbx_user1051 <sbx_user1051@169.254.13.49>
2025-07-10 20:22:43 +0000

    Thu Jul 10 20:22:43 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -7,15 +7,16 @@ import java.util.Map;
 
 public class CheckoutSolution {
 
-    Map<Character,Integer> individualSKUCost = new HashMap<>(
-            Map.of('A',50,
-                    'B',30,
-                    'C',20,
-                    'D',15)
+    Map<Character,Map<Integer,Integer>> skuCost = new HashMap<>(
+            Map.of('A',new HashMap<>(Map.of(1,50,3,130)),
+                    'B',new HashMap<>(Map.of(1,30,2,45)),
+                    'C',new HashMap<>(Map.of(1,20)),
+                    'D',new HashMap<>(Map.of(1,15)))
     );
 
     public Integer checkout(String skus) {
 
     }
 }
+
 
